af98b21af7b2851241601958489b884da4ed5ceb
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-09-06 22:47:30 +0200

    Update index.php

--- jour01/job02/index.php
+++ jour01/job02/index.php
@@ -5,23 +5,18 @@ function my_str_reverse($string) {
 	$result = "";
 	$index = 0;
 
-    // While the index of $string has a value
 	while (isset($string[$index])) {
-		// Increase the index by one to get all the characters of $string
 		$index++;
 	}
 
-	// Browse $string in reverse starting from the index
 	while ($index > 0) {
-		// Decrease the index by one to get all the characters of $string
 		$index--;
-		// Add the character at the index to the result
 		$result .= $string[$index];
 	}
 	
 	return $result;
 }
 
-echo my_str_reverse("Runtrack PHP");
+my_str_reverse("Runtrack PHP");
 
 ?>
