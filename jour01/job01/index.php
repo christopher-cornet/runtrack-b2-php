734fe8166ee5d40fce8cb0a3b79d944a5abe0ebc
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-09-06 22:48:04 +0200

    Update index.php

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -5,17 +5,13 @@ function my_str_search($character, $string) {
     $occurrences = 0;
     $index = 0;
 
-    // While the index of $string has a value
     while (isset($string[$index])) {
-        // If the character in $string is equal to the character $character, increment the occurrences variable
         if ($string[$index] === $character) {
           $occurrences++;
         }
-        // Increase index of one
         $index++;
     }
     
-    // Return the number of occurrences
     return $occurrences;
 }
 
