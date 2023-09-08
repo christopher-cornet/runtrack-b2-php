a21191f3fd1eec0fcf035fc7a7b76b6ddf70fc06
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-09-08 23:12:03 +0200

    Add job 4

--- jour-01/job04/index.php
+++ jour-01/job04/index.php
@@ -2,35 +2,29 @@
 
 function my_fizz_buzz($num) {
 
-	$result = [1];
-	$i = 1;
+	$result = [];
 
-	while ($num > 1) {
+	for ($i = 1; $i <= $num; $i++) {
 
-		$i++;
-		
-		if ($num % 3 == 0 && $num % 5 == 0) {
+		if ($i % 3 == 0 && $i % 5 == 0) {
 			array_push($result, "FizzBuzz");
 		}
-		elseif ($num % 3 == 0) {
+		elseif ($i % 3 == 0) {
 			array_push($result, "Fizz");
 		}
-		elseif ($num % 5 == 0) {
+		elseif ($i % 5 == 0) {
 			array_push($result, "Buzz");
 		}
 		else {
 			array_push($result, $i);
 		}
-		
-		var_dump($result);
-		$num--;
-		
+
 	}
 	
 	return $result;
 
 }
 
-my_fizz_buzz(10);
+my_fizz_buzz(15);
 
 ?>
